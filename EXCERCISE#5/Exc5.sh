518ceda94f52bed9ddc9c1963caad153efda68ad
KrishnenduAS <74754645+KrishnenduAS@users.noreply.github.com>
2021-09-28 23:53:07 +0530

    Create Exc5.sh

---
+++ EXCERCISE#5/Exc5.sh
@@ -0,0 +1,10 @@
+echo  "Check it is file or directory:" 
+if [ -d $1 ] 
+then
+	echo "$1 is a directory"
+elif [ -f $1 ] 
+then
+	echo "$1 is a file"
+else
+	echo "$1 is not a file nor a directory"
+fi
